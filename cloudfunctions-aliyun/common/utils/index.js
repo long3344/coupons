11aedfb884df9d2f3c40db297e4a5b1fbaf3495c
long3344 <1070597868@qq.com>
2021-02-25 16:24:07 +0800

    Update index.js

--- cloudfunctions-aliyun/common/utils/index.js
+++ cloudfunctions-aliyun/common/utils/index.js
@@ -1,2 +1,2 @@
-exports.APPID = 'wx9472d5ad54e879ed';  //这里是我的appid，需要改成你自己的
-exports.SECREAT = '7fefd4********65778a';   //密钥也要改成你自己的
+exports.APPID = 'wx6afc74399355ee03';  //这里是我的appid，需要改成你自己的
+exports.SECREAT = 'e53fa9c075****ccc4f1ff9';   //密钥也要改成你自己的
